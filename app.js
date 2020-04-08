07f81afe202316f1b0243bc88e72c74f9f113565
soyjne <soyjne@gmail.com>
2020-04-08 16:06:19 +0000

    modif app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Server Started!');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function () {
+
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+
 });
